ce3f779bc3d11e667810bde659c8d208d8d5eb3b
Polina Mishina <polya.nemchinova@gmail.com>
2023-10-22 18:44:50 +0300

    Fix methods

--- services/project-management-service/app/app.py
+++ services/project-management-service/app/app.py
@@ -1,48 +1,51 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from typing import Dict, Optional
+from schemas import Project, ProjectBase
+from typing import Dict
 
 app = FastAPI()
-
-
-class Project(BaseModel):
-    name: str
-    description: Optional[str] = None
-
 
 projects_db: Dict[int, Project] = {}
 
 
 @app.post("/projects", response_model=Project, summary='Добавляет проект в базу')
-def create_project(project: Project):
-    projects_db[len(projects_db) + 1] = project
-    return project
+def create_project(project: ProjectBase):
+    result = Project(
+        **project.model_dump(),
+        id=len(projects_db) + 1,
+    )
+    projects_db[result.id] = result
+    return result
 
 
-@app.get("/projects", response_model=Dict[int, Project], summary='Возвращает список проектов')
+@app.get("/projects", response_model=list[Project], summary='Возвращает список проектов')
 def get_projects():
-    return projects_db
+    return [v for k, v in projects_db.items()]
 
 
-@app.get("/projects/{project_id}", response_model=Project, summary='Возвращает информацию о проекте')
+@app.get("/projects/{project_id}", summary='Возвращает информацию о проекте')
 def get_project(project_id: int):
     if project_id in projects_db:
         return projects_db[project_id]
     return JSONResponse(status_code=404, content={"message": "Проект не найден"})
 
 
-@app.put("/projects/{project_id}", response_model=Project, summary='Обновляет информацию о проекте')
-def update_project(project_id: int, project: Project):
+@app.put("/projects/{project_id}", summary='Обновляет информацию о проекте')
+def update_project(project_id: int, project: ProjectBase):
     if project_id in projects_db:
-        projects_db[project_id] = project
+        result = Project(
+            **project.model_dump(),
+            id=project_id,
+        )
+        projects_db[project_id] = result
         return projects_db[project_id]
     return JSONResponse(status_code=404, content={"message": "Проект не найден"})
 
 
-@app.delete("/projects/{project_id}", response_model=Project, summary='Удаляет проект из базы')
+@app.delete("/projects/{project_id}", summary='Удаляет проект из базы')
 def delete_project(project_id: int):
     if project_id in projects_db:
+        result = projects_db[project_id]
         del projects_db[project_id]
-        return JSONResponse(status_code=200, content={"message": "Проект успешно удален"})
+        return result
     return JSONResponse(status_code=404, content={"message": "Проект не найден"})
